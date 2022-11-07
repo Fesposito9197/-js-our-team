1c3f22a5239e3c5cfe82c7a6ac1a1160accc0da4
Fesposito9197 <kekkoesp8@gmail.com>
2022-11-07 16:14:02 +0100

    crea array di oggetti

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// creo un array di oggetti 
+
+const team = [
+    {
+        name : "Wayne Barnett",
+        position : "Founder & CEO",
+        profilePhoto : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name : "Angela Caroll	",
+        position : "Chief Editor",
+        profilePhoto : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name : "Walter Gordon",
+        position : "Office Manager",
+        profilePhoto : "walter-gordon-office-manager.jpg"
+    },
+    {
+        name : "Angela Lopez",
+        position : "Social Media Manager",
+        profilePhoto : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name : "Scott Estrada",
+        position : "Developer",
+        profilePhoto : "scott-estrada-developer.jpg"
+    },
+    {
+        name : "Barbara Ramos",
+        position : "Graphic Designer",
+        profilePhoto : "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+// stampare in console per ogni membro del team le informazioni contenute nell'oggetto
